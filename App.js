b0a828031118e254f55437fe68b2cc435db64265
Per-Andre Stromhaug <pstromhaug@managedbyq.com>
2018-05-19 18:00:10 -0400

    wip, adding createUser

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, AsyncStorage } from 'react-native';
 
 import Amplify from 'aws-amplify';
 import aws_exports from './src/aws-exports';
-import { withAuthenticator } from 'aws-amplify-react-native';
+import { Authenticator } from 'aws-amplify-react-native';
 
 import AWSAppSyncClient from 'aws-appsync';
 import { Rehydrated } from 'aws-appsync-react';
@@ -15,6 +15,8 @@ import { graphql, ApolloProvider, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import createMessage from './src/graphql/mutations/createMessage';
+import createUser from './src/graphql/mutations/createUser';
+import getMe from './src/graphql/queries/getMe';
 
 
 Amplify.configure(aws_exports)
@@ -32,51 +34,99 @@ const client = new AWSAppSyncClient({
 
 
 class App extends React.Component {
-  render() {
-    console.log('props again', this.props)
-    this.props.createMessage({
-        content: 'hi',
-        conversationId: "convo ID",
-        createdAt: '13 PM',
-    })
-    return (
-        <View style={styles.container}>
-            <Text>hiiiiyyyy. Open up App.js to start working on your app!</Text>
-            <Text>Changes you make will automatically reload.</Text>
-            <Text>Shake your phone to open the developer menu.</Text>
-        </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        Auth.currentSession().then(session => {
+          this.logInfoToConsole(session);
+          this.state =  {session};
+          this.register();
+          this.createUser();
+        });
+    }
+
+    register() {
+        client.watchQuery({
+            query: getMe,
+            fetchPolicy: 'cache-only'
+        }).subscribe(({data}) => {
+            if (data) {
+                console.log('me data', data)
+            }
+        });
+    }
+
+    logInfoToConsole(session) {
+        console.log(session);
+        console.log(`ID Token: <${session.idToken.jwtToken}>`);
+        console.log(`Access Token: <${session.accessToken.jwtToken}>`);
+        console.log('Decoded ID Token:');
+        console.log(JSON.stringify(session.idToken.payload, null, 2));
+        console.log('Decoded Acess Token:');
+        console.log(JSON.stringify(session.accessToken.payload, null, 2));
+    }
+
+    _sendMessage() {
+        this.props.createMessage({
+            content: 'hi2',
+            conversationId: "convo ID2",
+            createdAt: '13 PM',
+        })
+    }
+
+    render() {
+        return (
+            <TouchableOpacity onPress={this._sendMessage.bind(this)}>
+                <View style={styles.container}>
+                    <Text>hiiiiyyyy. Open up App.js to start working on your app!</Text>
+                    <Text>Changes you make will automatically reload.</Text>
+                    <Text>Shake your phone to open the developer menu.</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
 }
 
 const AppGraphQL = compose(
     graphql(createMessage, {
         props: (props) => ({
             createMessage: (message) => {
-                console.log('mutating')
                 props.mutate({
                     variables: {
+                        id: 'id1',
                         content: message.content,
                         createdAt: message.createdAt,
                         conversationId: message.conversationId,
                     }
-                }).then( data => console.log('data', data))
+                }).then( data => console.log('createMessage data:', data))
             }
         })
     }),
+    graphql(createUser, {
+        createUser: (user) => {
+            console.log('createUse session', this.state.session);
+            props.mutate({
+                variables: {
+                    username: this.state.session.idToken.payload['cognito:username'],
+                    id: this.state.session.idToken.payload['sub'],
+                    cognitoId: this.state.session.idToken.payload['sub'],
+                    registered: false
+                }
+            })
+        }
+    })
 )(App);
 
-class WithProvider extends React.Component {
+class withAuthentication extends React.Component {
   render() {
-
-    console.log('props', this.props)
-
     return (
-        <ApolloProvider client={client}>
-            <Rehydrated>
-                <AppGraphQL/>
-            </Rehydrated>
-        </ApolloProvider>
+        <Authenticator>
+            <ApolloProvider client={client}>
+                <Rehydrated>
+                    <AppGraphQL/>
+                </Rehydrated>
+            </ApolloProvider>
+        </Authenticator>
     );
   }
 }
@@ -90,4 +140,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default withAuthenticator(WithProvider);
+export default withAuthentication
