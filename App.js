a7d9009c042ca5ff367361d6f755a4121a51ce50
Per-Andre Stromhaug <pstromhaug@managedbyq.com>
2018-06-10 10:52:08 -0400

    finally got it to not crash

--- App.js
+++ App.js
@@ -1,54 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, AsyncStorage } from 'react-native';
+import { View, Text } from 'react-native';
 
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './src/aws-exports';
-import { withAuthenticator } from 'aws-amplify-react-native';
-import { createStackNavigator } from 'react-navigation';
+// import Amplify, { Auth } from 'aws-amplify';
+// import aws_exports from './src/aws-exports';
+// import { withAuthenticator } from 'aws-amplify-react-native';
+// // import { createStackNavigator } from 'react-navigation';
 
-import AWSAppSyncClient from 'aws-appsync';
-import { Rehydrated } from 'aws-appsync-react';
-import appSyncConfig from './src/AppSync.js';
-import { AUTH_TYPE } from 'aws-appsync/lib/link/auth-link';
-import { graphql, ApolloProvider, compose } from 'react-apollo';
+// import AWSAppSyncClient from 'aws-appsync';
+// import { Rehydrated } from 'aws-appsync-react';
+// import appSyncConfig from './src/AppSync.js';
+// import { AUTH_TYPE } from 'aws-appsync/lib/link/auth-link';
+// import { ApolloProvider } from 'react-apollo';
 
-import createMessage from './src/graphql/mutations/createMessage';
-
-import ConversationList from './src/components/ConversationList';
-import CreateConversation from './src/components/CreateConversation';
-import Messages from './src/components/Messages';
+// import ConversationList from './src/components/ConversationList';
+// // import CreateConversation from './src/components/CreateConversation';
+// // import Messages from './src/components/Messages';
 
 
-Amplify.configure(aws_exports)
+// Amplify.configure(aws_exports)
 
-console.disableYellowBox = true;
+// console.disableYellowBox = true;
 
-const client = new AWSAppSyncClient({
-    url: appSyncConfig.graphqlEndpoint,
-    region: appSyncConfig.region,
-    auth: {
-        type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
-        jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken()
-    }
-});
+// const client = new AWSAppSyncClient({
+//     url: appSyncConfig.graphqlEndpoint,
+//     region: appSyncConfig.region,
+//     auth: {
+//         type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+//         jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken()
+//     }
+// });
 
-const AppNavigation = createStackNavigator(
-    { Conversations: ConversationList,
-      CreateConversation: CreateConversation,
-      Messages: Messages },
-    { initialRouteName: 'Conversations'}
-);
+// // const AppNavigation = createStackNavigator(
+// //     { Conversations: ConversationList },
+// //       // CreateConversation: CreateConversation,
+// //       // Messages: Messages },
+// //     { initialRouteName: 'Conversations'}
+// // );
 
-class withProvidor extends React.Component {
-  render() {
-    return (
-        <ApolloProvider client={client}>
-            <Rehydrated>
-                <AppNavigation/>
-            </Rehydrated>
-        </ApolloProvider>
-    );
-  }
-}
+// class withProvidor extends React.Component {
+//   render() {
+//     return (
+//         <ApolloProvider client={client}>
+//             <Rehydrated>
+//                 <ConversationList />
+//             </Rehydrated>
+//         </ApolloProvider>
+//     );
+//   }
+// }
 
-export default withAuthenticator(withProvidor)
+// export default withAuthenticator(withProvidor)
+
+const comp = () => (
+  <View>
+    <Text> hiiii </Text>
+  </View>
+)
+
+export default comp;
--- src/components/ConversationList.js
+++ src/components/ConversationList.js
@@ -1,96 +1,28 @@
 import React from 'react';
-import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { graphql, ApolloProvider, compose } from 'react-apollo';
+import { View } from 'react-native';
+import { graphql, compose } from 'react-apollo';
 
-import { Auth } from 'aws-amplify';
+// import { Auth } from 'aws-amplify';
 
-import createUser from '../graphql/mutations/createUser';
-import getUserConversationsConnection from '../graphql/queries/getUserConversationsConnection';
+// import createUser from '../graphql/mutations/createUser';
+// import getUserConversationsConnection from '../graphql/queries/getUserConversationsConnection';
 import getMe from '../graphql/queries/getMe';
 
 
-class   ConversationList extends React.Component {
-
-    constructor(props) {
-        super(props);
-        Auth.currentSession().then(session => {
-            props.createUser({
-                username: session.idToken.payload['cognito:username'],
-                id: session.idToken.payload['sub'],
-                cognitoId: session.idToken.payload['sub'],
-            });
-        });
-    }
-
-    _navigateToMessages(conversationId) {
-        return () => {
-            console.log('navigating to ', conversationId)
-            this.props.navigation.navigate('Messages', { conversationId })
-        }
-    }
-
-    _renderItem(item) {
-        conversationId = item.item.conversation.id;
-        conversationName = item.item.conversation.name;
-        return (
-            <TouchableOpacity onPress={ this._navigateToMessages(conversationId) }>
-                <Text>{conversationName}</Text>
-            </TouchableOpacity>
-        )
-    }
+class ConversationList extends React.Component {
 
     render() {
-        console.log('conversationList render props: ', this.props)
+        console.log('conversationList render props: ', this.props.data)
         return (
             <View>
-                <FlatList
-                    data={this.props.conversationConnection}
-                    renderItem={this._renderItem.bind(this)} />
-                <Button
-                    onPress={() => this.props.navigation.navigate('CreateConversation')}
-                    title='Create Conversation'/>
+              hiii
             </View>
         )
     }
 }
 
 const ConversationListGraphQL = compose(
-    graphql(createUser, {
-        props: (props) => ({
-            createUser: (user) => {
-                props.mutate({
-                    variables: {
-                        username: user.username,
-                        id: user.id,
-                        cognitoId: user.cognitoId,
-                        registered: false
-                    }
-                }).then( data => {
-                    console.log('createUser data: ', data);
-                })
-            }
-        })
-    }),
-    graphql(getMe, {
-        props: (props) => {
-            if (props.data.me !== undefined) {
-                console.log('getMe props: ', props)
-                return {
-                    cognitoId: props.data.me.cognitoId,
-                }
-            } else {
-                console.log('getMe props no me: ', props)
-            }
-        }
-    }),
-    graphql(getUserConversationsConnection, {
-        props: (props) => {
-            console.log('getUserConversationsConnection props: ', props)
-            return {
-                conversationConnection: props.data.me.conversations.userConversations,
-            }
-        }
-    }),
+    graphql(getMe),
 )(ConversationList)
 
 export default ConversationListGraphQL;
